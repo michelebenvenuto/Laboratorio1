e1305a165197c27bd750b82a907b2517a656e084
michelebenveuto <35434145+michelebenveuto@users.noreply.github.com>
2019-01-11 16:55:36 -0600

    Artributos de la clase radio de carro

--- src/CarRadio.java
+++ src/CarRadio.java
@@ -1,2 +1,17 @@
+import java.lang.reflect.Array;
+
 public class CarRadio {
+    private boolean isOn;
+    private boolean isFM;
+    private double currentStationFM;
+    private double currentStationAM;
+    private double[] savedStationsFM;
+    private double[] savedStationsAM;
+    //Constructor
+    public CarRadio(){
+        this.isOn=false;
+        this.isFM=true;
+        this.currentStationFM=87.9;
+        this.currentStationAM=530;
+    }
 }
